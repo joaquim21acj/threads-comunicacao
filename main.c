dfdb91cc4ca54c617e9057050e5a3c961d9c4363
Joaquim Alves <joaquimjunior13@gmail.com>
2019-11-15 18:15:00 -0300

    Adição de cada thread a sua posição e preenchimento do mapa

--- main.c
+++ main.c
@@ -11,6 +11,14 @@ struct no
 //redefinindo a struct para encurtar o comando na declaração
 typedef struct no no ;
 
+//Estrutura que define o mapa de onde a thread vai atuar
+struct atuacao{
+int linha[3];
+int coluna[3];
+int posicaoX, posicaoY;
+};
+//definição da área de atuação
+typedef struct atuacao atuacao;
 
 /*Rotina que será executada*/
 void * routine(void *arg);
@@ -21,72 +29,62 @@ int main (int argc, char *argv[])
 {
     no matriz[30][30];
     int i = preencheTela(matriz);
-
-    pthread_t thread_idA;
-    pthread_t thread_idB;
-    void * thread_res;
-    int rstatus;
-
-    /*tenta iniciar o thread, indicando a função 'routine' e passando como argumento a string "Thread A"*/
-    rstatus = pthread_create(&thread_idA, NULL, routine, (void*)("Thread A"));
-
-    /*verificar se ocorreu algum erro na chamada de pthread_create*/
-    if(rstatus != 0)
-    {
-        printf ("Erro ao criar o thread A.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf ("Thread A criado com sucesso!\n");
-
-    /*tenta iniciar o thread, indicando novamente a função 'routine' e passando como argumento a string "Thread B"*/
-    rstatus = pthread_create (&thread_idB, NULL, routine, (void*)("Thread B"));
-
-    /*verificar se ocorreu algum erro na chamada de pthread_create*/
-    if(rstatus != 0)
-    {
-        printf ("Erro ao criar o thread B.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf ("Thread B criado com sucesso!\n");
-
-
-    /*aguarda finalização do thread A identificado por thread_idA. O retorno é passado pelo ponteiro thread_res*/
-    rstatus = pthread_join (thread_idA, &thread_res);
-
-    if(rstatus != 0)
-    {
-        printf ("Erro ao aguardar finalização do thread A.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf ("Thread A finalizado! Retorno = %s\n", (char *)thread_res);
-
-
-    /*aguarda finalização do thread B identificado por thread_idB. O retorno é passado pelo ponteiro thread_res*/
-    rstatus = pthread_join (thread_idB, &thread_res);
-
-    if(rstatus != 0)
-    {
-        printf ("Erro ao aguardar finalização do thread B.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf ("Thread B finalizado! Retorno = %s\n", (char *)thread_res);
+    int a;
+//    /*tenta iniciar o thread, indicando a função 'routine' e passando como argumento a string "Thread A"*/
+//    rstatus = pthread_create(&thread_idA, NULL, routine, (void*)("Thread A"));
+//
+//    /*verificar se ocorreu algum erro na chamada de pthread_create*/
+//    if(rstatus != 0)
+//    {
+//        printf ("Erro ao criar o thread A.\n");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    printf ("Thread A criado com sucesso!\n");
+//
+//    /*tenta iniciar o thread, indicando novamente a função 'routine' e passando como argumento a string "Thread B"*/
+//    rstatus = pthread_create (&thread_idB, NULL, routine, (void*)("Thread B"));
+//
+//    /*verificar se ocorreu algum erro na chamada de pthread_create*/
+//    if(rstatus != 0)
+//    {
+//        printf ("Erro ao criar o thread B.\n");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    printf ("Thread B criado com sucesso!\n");
+//
+//
+//    /*aguarda finalização do thread A identificado por thread_idA. O retorno é passado pelo ponteiro thread_res*/
+//    rstatus = pthread_join (thread_idA, &thread_res);
+//
+//    if(rstatus != 0)
+//    {
+//        printf ("Erro ao aguardar finalização do thread A.\n");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    printf ("Thread A finalizado! Retorno = %s\n", (char *)thread_res);
+//
+//
+//    /*aguarda finalização do thread B identificado por thread_idB. O retorno é passado pelo ponteiro thread_res*/
+//    rstatus = pthread_join (thread_idB, &thread_res);
+//
+//    if(rstatus != 0)
+//    {
+//        printf ("Erro ao aguardar finalização do thread B.\n");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    printf ("Thread B finalizado! Retorno = %s\n", (char *)thread_res);
     return 0;
 }
 
 void * routine(void *arg)
 {
-    int contador = 10;
-
-    /*procedimento para decrementar um contador e exibir o seu valor*/
-    while(contador--)
-    {
-        printf("%s: %i\n", (char *)arg, contador);
-    }
-
+    atuacao *thr;
+    thr = (atuacao *)arg;
+    printf("Posicao da thread X: %d Y: %d\n", thr->posicaoX, thr->posicaoY);
     /*finaliza a função retornando o argumento que foi recebido*/
     pthread_exit(arg);
 }
@@ -101,6 +99,29 @@ int preencheTela(no matriz[30][30])
             if((isThread(i)==1) && (isThread(j)==1))
             {
                 matriz[i][j].thread=1;
+                atuacao area;
+                area.posicaoX = i;
+                area.posicaoY = j;
+                // vai da linha anterior à thread até a linha posterior
+                int posicao = 0;
+                for(int x=(i-1); x<=(i+1); x++){
+                  area.linha[posicao] = x;
+                  posicao++;
+                }
+                // vai da coluna anterior à thread até a coluna posterior
+                posicao = 0;
+                for(int y=(j-1); y<=(j+1); y++){
+                    area.coluna[posicao] = y;
+                    posicao++;
+                }
+                // criação da thread
+                pthread_t thread_id;
+                void * thread_res;
+                int rstatus;
+
+                atuacao *ptArea = &area;
+                rstatus = pthread_create (&thread_id, NULL, routine, (void*)(ptArea));
+
             }
         }
     }
